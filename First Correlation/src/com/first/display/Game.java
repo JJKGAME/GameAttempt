3d1dd851731395e5d41df9d59fd053f5412afa5f
jojo014 <vanauken.joel@yahoo.com>
2013-09-29 14:32:07 -0400

    with an e

--- First Correlation/src/com/first/display/Game.java
+++ First Correlation/src/com/first/display/Game.java
@@ -60,7 +60,7 @@ public class Game extends Canvas implements Runnable {
 		g.setColor(Color.RED);
 		Font f = new Font("sansserif", Font.BOLD, 32);
 		g.setFont(f);
-		g.drawString("This is frustrating", getWidth()/2 + 4,getHeight()/2);
+		g.drawString("This is MADDENENG", getWidth()/2 + 4,getHeight()/2);
 		g.dispose();
 		bs.show();
 	}
